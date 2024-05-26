a080623585dd241425432a598452b3ee99eaeb91
Kelton <keltonfernandes@live.com>
2024-05-26 10:36:13 -0100

    refactor: improve page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Container, Typography, Button, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-function Home() {
+const Home = () => {
     return (
-        <h1 style={{ color: 'black' }}>Home</h1>
-    )
-}
+        <Container>
+            <Box display="flex" flexDirection="column" alignItems="center" mt={5}>
+                <Typography variant="h2" color="white" style={{ margin: 10, marginBottom: '4%' }}>Eleos: Frontend developer exercise</Typography>
+                <Typography variant="h3" color="white" gutterBottom>Welcome to the Home Page</Typography>
+                <Button variant="contained" component={Link} to="/user" style={{ margin: '10px' }}>
+                    Go to User Page
+                </Button>
+                <Button variant="contained" color="secondary" component={Link} to="/task" style={{ margin: '10px' }}>
+                    Go to Task Page
+                </Button>
+            </Box>
+        </Container>
+    );
+};
 
-export default Home
+export default Home;
